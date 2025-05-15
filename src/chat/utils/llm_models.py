b6113b070495e195fa572a340d3c3e13bf36259d
jitenderfoundry <jitender@truefoundry.com>
2025-05-15 11:53:41 +0530

    fixed the gateway issue

--- src/chat/utils/llm_models.py
+++ src/chat/utils/llm_models.py
@@ -5,10 +5,8 @@ from time import perf_counter
 from typing import Dict, Tuple, Any
 
 from langchain_google_vertexai import ChatVertexAI
-from langchain_openai.chat_models.base import ChatOpenAI
-from langchain_openai import AzureChatOpenAI
+from langchain_openai import AzureChatOpenAI , ChatOpenAI
 from langchain.schema import HumanMessage, SystemMessage
-from langchain_community.chat_models import ChatOpenAI
 
 from src.common.config.app_config import get_application_config
 from src.common.service.logging.logger import error, info
@@ -18,13 +16,15 @@ CONFIG = get_application_config()
 
 def get_truefoundry_llm():
     return ChatOpenAI(
-        model="openai-main/gpt-4o",
-        temperature=0.7,
-        max_tokens=2500,
-        streaming=False,
-        openai_api_key=os.getenv("TFY_API_KEY"),
-        base_url=os.getenv("LLM_BASE_URL"),
+        model="openai-main/o3-mini",
+        streaming=True,
+        api_key=os.getenv("TFY_API_KEY"), 
+        base_url=os.getenv("GATEWAY_URL"),
+        extra_headers   =  {
+            "X-TFY-METADATA": '{"tfy_log_request":"true"}',
+        }
     )
+
 
 class AzureOpenAI(DeepEvalBaseLLM):
     """
